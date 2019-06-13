5c8e67da3fb5f422416d37b0860f30f7d7ccdd54
lipuyu <puyu.li@youpeng-inc.com>
2019-06-13 17:03:16 +0800

    add db

--- conf/orm.go
+++ conf/orm.go
@@ -12,7 +12,10 @@ var Orm *xorm.Engine
 func LoadOrm(){
 	if Orm == nil {
 		var err error
-		Orm, err = xorm.NewEngine("mysql", "root:@/imgo")
+		Orm, err = xorm.NewEngine(
+			"mysql",
+			"stouch:Jibuzhu123@rm-2zew4kr6drni3qkok.mysql.rds.aliyuncs.com/stouch",
+			)
 		if err != nil {
 		}
 		iris.RegisterOnInterrupt(func() {
