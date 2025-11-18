f11035c6d463b6f262dcbc47693196edf234cebd
lipuyu <827893047@qq.com>
2025-11-18 11:53:51 +0800

    增加设备接口

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"stouch_server/src/appconf"
 	"stouch_server/src/auth"
@@ -16,6 +15,8 @@ import (
 	"stouch_server/src/test"
 	"stouch_server/src/websock/service"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
